56acc99c962f892f5958c246f39a2d3bce212b2e
kareka78 <47817807+kareka78@users.noreply.github.com>
2019-10-27 20:50:06 +0100

    Introducir elementos en spinner de Activity Filtrar

--- app/src/main/java/com/example/drawcoco/Filtrar.java
+++ app/src/main/java/com/example/drawcoco/Filtrar.java
@@ -3,12 +3,19 @@ package com.example.drawcoco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Filtrar extends AppCompatActivity {
-
+    Spinner eligecategoria = (Spinner) findViewById(R.id.eligecategoria);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtrar);
+        String[] datos = new String[] {"Paisajes", "Animales", "Personas"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, datos);
+        eligecategoria.setAdapter(adapter);
     }
+
 }
