5f1e10010b8899365d3747fdf7f595caa2645bf0
kareka78 <47817807+kareka78@users.noreply.github.com>
2019-10-27 21:25:57 +0100

    Cargar valores spinner de Acticity Registro

--- app/src/main/java/com/example/drawcoco/Registro.java
+++ app/src/main/java/com/example/drawcoco/Registro.java
@@ -3,12 +3,18 @@ package com.example.drawcoco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Registro extends AppCompatActivity {
-
+    Spinner genero=(Spinner)findViewById(R.id.eligegenero);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        String[] sexo = new String[] {"Mujer", "Hombre", "Otros"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, sexo);
+        genero.setAdapter(adapter);
     }
 }
