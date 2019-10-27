d5e899001b9518a83e33e56eb65519f8cb524486
kareka78 <47817807+kareka78@users.noreply.github.com>
2019-10-27 22:11:02 +0100

    Introducida la condicion de gratuita en el Activity Filtrar

--- app/src/main/java/com/example/drawcoco/Filtrar.java
+++ app/src/main/java/com/example/drawcoco/Filtrar.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 
 public class Filtrar extends AppCompatActivity {
     Spinner eligecategoria = (Spinner) findViewById(R.id.eligecategoria);
+    CheckBox gratuitas=(CheckBox)findViewById(R.id.gratuitas);
+    CheckBox menor=(CheckBox)findViewById(R.id.menorprecio);
+    CheckBox mayor=(CheckBox)findViewById(R.id.mayorprecio);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,11 @@ public class Filtrar extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, datos);
         eligecategoria.setAdapter(adapter);
+         if(gratuitas.isChecked()){
+            menor.setEnabled(false);
+            mayor.setEnabled(false);
+         }
+
     }
 
 }
