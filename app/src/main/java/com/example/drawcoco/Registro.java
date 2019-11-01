197ca4d2a30531a1229d85cc835ba32a4fa55c8d
kareka78 <47817807+kareka78@users.noreply.github.com>
2019-11-01 15:31:24 +0100

    Pequeños comentarios en Activity Registro

--- app/src/main/java/com/example/drawcoco/Registro.java
+++ app/src/main/java/com/example/drawcoco/Registro.java
@@ -5,19 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.drawcoco.clases.Personas;
+
+//Este Activity recopilará los datos de nuestros nuevos clientes y los alojará en una base de datos.
 
 public class Registro extends AppCompatActivity {
-    Spinner genero;
+    Spinner sexo;
+    String nickname;
+    String nombre;
+    String apellidos;
+    String mail;
+    String descripcion;
+    String contraseña;
+    Personas.Genero genero;
+    float dinero;
+    TextView txtnickname;
+    TextView txtnombre;
+    TextView txtapellidos;
+    TextView txtmail;
+    TextView txtcontraseña;
+    TextView txtgenero;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        genero=findViewById(R.id.eligegenero);
+        sexo=findViewById(R.id.eligegenero);
 
-        String[] sexo = new String[] {"Mujer", "Hombre", "Otros"};
+        String[] gen = new String[] {"Mujer", "Hombre", "Otros"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, sexo);
-        genero.setAdapter(adapter);
+                android.R.layout.simple_spinner_item, gen);
+        sexo.setAdapter(adapter);
+
+
+        txtnickname=this.findViewById(R.id.textoNickName);
+        txtnombre=this.findViewById(R.id.textoNombre);
+        txtapellidos=this.findViewById(R.id.textoApellidos);
+        txtmail=this.findViewById(R.id.textoEmail);
+        txtcontraseña=this.findViewById(R.id.textoPassword);
+        txtgenero=this.findViewById(R.id.eligegenero);
+
+        nickname= (String)txtnickname.getText();
+        nombre=(String)txtnombre.getText();
+        apellidos=(String)txtapellidos.getText();
+        mail=(String)txtmail.getText();
+        contraseña=(String)txtcontraseña.getText();
+        
+
+        //Captamos los datos a través de la clase persona, creando una nueva persona con esos datos que
+        //posteriormente almacenaremos en la base de datos.
+        Personas persona=new Personas(nickname,nombre,apellidos,mail,contraseña,genero,dinero);
     }
 }
