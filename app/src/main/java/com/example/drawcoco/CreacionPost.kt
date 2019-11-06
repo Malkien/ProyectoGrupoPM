cc11d0fcc15546828fc0cde4f3754f29c67aebf3
Malkien <kevin1cuenca@gmail.com>
2019-11-06 22:05:48 +0100

    Agregado creacionPost

---
+++ app/src/main/java/com/example/drawcoco/CreacionPost.kt
@@ -0,0 +1,12 @@
+package com.example.drawcoco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreacionPost : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creacion_post)
+    }
+}
