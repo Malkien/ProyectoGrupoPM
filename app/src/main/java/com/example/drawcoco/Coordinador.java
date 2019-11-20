68e3a489b4c02717c5926568d77b1a2b38d40cd4
Malkien <kevin1cuenca@gmail.com>
2019-11-20 20:13:43 +0100

    Implementado Galeria a navegation

--- app/src/main/java/com/example/drawcoco/Coordinador.java
+++ app/src/main/java/com/example/drawcoco/Coordinador.java
@@ -1,23 +1,40 @@
 package com.example.drawcoco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.drawcoco.adapters.AdapterPaginas;
+import com.example.drawcoco.clases.Cliente;
+import com.example.drawcoco.clases.Creador;
+import com.example.drawcoco.clases.Imagen;
+import com.example.drawcoco.clases.Suscripcion;
 import com.google.android.material.tabs.TabLayout;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static com.example.drawcoco.clases.Personas.Genero.HOMBRE;
+import static com.example.drawcoco.clases.Personas.Genero.MUJER;
 
 public class Coordinador extends AppCompatActivity{
     private AdapterPaginas adapterPaginas;//Adaptador de las paginas
     private ViewPager viewPager;//Donde se muestan las paginas
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinador);
         adapterPaginas=new AdapterPaginas(getSupportFragmentManager());
+
         viewPager=findViewById(R.id.contenedorFragment);
 
         setupViewPager();
@@ -37,7 +54,7 @@ public class Coordinador extends AppCompatActivity{
     private void setupViewPager() {
 
         adapterPaginas.addFragment(new Inicio());
-        adapterPaginas.addFragment(new Inicio());
+        adapterPaginas.addFragment(new Galeria());
         adapterPaginas.addFragment(new Inicio());
         adapterPaginas.addFragment(new Inicio());
         adapterPaginas.addFragment(new Inicio());
--- app/src/main/java/com/example/drawcoco/Galeria.java
+++ app/src/main/java/com/example/drawcoco/Galeria.java
@@ -1,42 +1,68 @@
 package com.example.drawcoco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.TextView;
 
 import com.example.drawcoco.adapters.AdapterImagenGaleria;
 import com.example.drawcoco.clases.Creador;
+import com.example.drawcoco.clases.Imagen;
 
-public class Galeria extends AppCompatActivity {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Galeria extends Fragment {
 
     TextView nombreArtista;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_galeria);
+    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.activity_galeria, container, false);
 
-        nombreArtista = this.findViewById(R.id.nombreArtistaGaleria);
 
-        Creador artista1 = (Creador) getIntent().getExtras().getSerializable("artista1");
+        nombreArtista = view.findViewById(R.id.nombreArtistaGaleria);
 
-        nombreArtista.setText(artista1.getNickname());
+        /////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////INFORMACION NECESARIA; HAZ LO QUE QUIERAS CON ELLO/////////
+        /////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<String> arrayTags1 = new ArrayList<>();
+        arrayTags1.add("Infantil");
+        arrayTags1.add("Bonito");
+
+        ArrayList<String> arrayTags2 = new ArrayList<>();
+        arrayTags2.add("Sexy");
+        arrayTags2.add("Hot");
+        ArrayList<Imagen> galeriaArtista1=new ArrayList<Imagen>();
+        galeriaArtista1 = new ArrayList<Imagen>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            galeriaArtista1.add(new Imagen("Pollo Dorado", "Un pollo bonito", 50, LocalDateTime.now(), "android.resource://" + getActivity().getPackageName() + "/" + R.drawable.pollodorado, arrayTags1, null, true, null));
+            galeriaArtista1.add(new Imagen("Poring", "Esta blandito", 12, LocalDateTime.now(), "android.resource://" + getActivity().getPackageName() + "/" + R.drawable.poi, arrayTags1, null, true, null));
+            galeriaArtista1.add(new Imagen("Bunny", "Conejita Sexy", 100, LocalDateTime.now(), "android.resource://" + getActivity().getPackageName() + "/" + R.drawable.bunny, arrayTags2, null, true, null));
+
+        }
+        ////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////
 
         //Llamamos al adapter de galeria para rellenar el GridView con las imagenes del artista seleccionado.
-        AdapterImagenGaleria adapterGaleria = new AdapterImagenGaleria(this,artista1.getImagenArrayList());
-        GridView grid = findViewById(R.id.gridViewGaleria);
+        AdapterImagenGaleria adapterGaleria = new AdapterImagenGaleria(view.getContext(),galeriaArtista1);//te he dejado el array de imagenes
+        GridView grid = view.findViewById(R.id.gridViewGaleria);
         grid.setAdapter(adapterGaleria);
-
+        return view;
     }
 
     //Boton que nos lleva a la activity_filtrar.
     public void botonGaleriaFiltrar(View view) {
-        Intent irAFiltrar = new Intent(this, Filtrar.class);
+        Intent irAFiltrar = new Intent(view.getContext(), Filtrar.class);
         startActivity(irAFiltrar);
     }
 }
